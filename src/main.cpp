5394bf4f8194e4bc263963c0eac5371abc3229f7
myeiter <Aceplace1116@gmail.com>
2023-12-14 12:26:25 -0500

    Set up for raylib on laptop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,25 @@
-#include <iostream>
+#include "raylib.h"
 
 int main() {
+    // Determin the Game Window Width and Height
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    std::cout<<"Hello";
-    std::cout<<"Hello";
-    std::cout<<"Hello";
+    // Initialize the Window
+    InitWindow(screenWidth, screenHeight, "Scout Card Maker");
+
+    // Setting the Frames Per Second
+    SetTargetFPS(60);
+
+    // The Game Loop
+    while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
+        BeginDrawing();
+        
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats Dude", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
 }
