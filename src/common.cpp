446b69e0871a023c220c6653c8ae26c5988312f7
myeiter <Aceplace1116@gmail.com>
2023-12-16 08:22:41 -0500

    Starting to migrate over scm 2023

---
+++ src/common.cpp
@@ -0,0 +1,245 @@
+#include <string>
+#include <stdexcept>
+namespace ScoutCardMaker2024 
+{
+
+
+enum class PlayerSide {OFFENSE = 0, DEFENSE = 1};
+enum class PlayerTag {NONE, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, LT, LG, OC,  RG, RT, OQ, S1, S2, S3, S4, S5};
+
+bool is_offensive_lineman_not_center(PlayerTag player_tag)
+{
+        return  player_tag == PlayerTag::LT ||
+                player_tag == PlayerTag::LG ||
+                player_tag == PlayerTag::RG ||
+                player_tag == PlayerTag::RT;
+}
+
+
+enum class Direction {RIGHT, LEFT, FORWARD};
+enum class DirectionStringLength { SINGLE_LETTER, SHORT, FULL};
+enum class DirectionStringCapitalization { FIRST_LETTER, LOWER_CASE, UPPER_CASE};
+
+
+float dir_factor(Direction direction) 
+{
+    return direction == Direction::RIGHT ? 1 : -1;
+}
+
+Direction opposite(Direction direction) 
+{
+    return direction == Direction::RIGHT ? Direction::LEFT : Direction::RIGHT;
+}
+
+enum class Direction { RIGHT, LEFT };
+enum class DirectionStringLength { SINGLE_LETTER, SHORT, FULL };
+enum class DirectionStringCapitalization { FIRST_LETTER, LOWER_CASE, UPPER_CASE };
+
+std::string get_direction_str(  Direction direction, DirectionStringLength direction_string_length,
+                                DirectionStringCapitalization direction_string_capitalization) 
+{    
+    if (direction == Direction::RIGHT) 
+    {
+        switch (direction_string_length) 
+        {
+            case DirectionStringLength::SINGLE_LETTER:
+            {
+                switch (direction_string_capitalization) 
+                {
+                    case DirectionStringCapitalization::FIRST_LETTER: return "R";
+                    case DirectionStringCapitalization::LOWER_CASE: return "r";
+                    default: return "R";
+                }
+            }
+            case DirectionStringLength::SHORT:
+            {
+                switch (direction_string_capitalization) 
+                {
+                    case DirectionStringCapitalization::FIRST_LETTER: return "Rt";
+                    case DirectionStringCapitalization::LOWER_CASE: return "rt";
+                    default: return "RT";
+                }
+            }
+            default: 
+            {
+                switch (direction_string_capitalization) 
+                {
+                    case DirectionStringCapitalization::FIRST_LETTER: return "Right";
+                    case DirectionStringCapitalization::LOWER_CASE: return "right";
+                    default: return "RIGHT";
+                }
+            }
+        }
+    } 
+    else 
+    {
+        switch (direction_string_length) 
+        {
+            case DirectionStringLength::SINGLE_LETTER:
+            {
+                switch (direction_string_capitalization) 
+                {
+                    case DirectionStringCapitalization::FIRST_LETTER: return "L";
+                    case DirectionStringCapitalization::LOWER_CASE: return "l";
+                    default: return "L";
+                    
+                }
+            }
+            case DirectionStringLength::SHORT:
+            {
+                switch (direction_string_capitalization) 
+                {
+                    case DirectionStringCapitalization::FIRST_LETTER: return "Lt";
+                    case DirectionStringCapitalization::LOWER_CASE: return "lt";
+                    default: return "LT";
+                    
+                }
+            }
+            default: // DirectionStringLength::FULL
+            {
+                switch (direction_string_capitalization) 
+                {
+                    case DirectionStringCapitalization::FIRST_LETTER: return "Left";
+                    case DirectionStringCapitalization::LOWER_CASE: return "left";
+                    default: return "LEFT";
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+}
+
+
+
+/*
+
+using OpenTK.Mathematics;
+
+namespace ScoutCardMaker2023.scm;
+
+public enum PlayerSide {OFFENSE = 0, DEFENSE = 1}
+public enum PlayerTag {NONE, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, LT, LG, OC,  RG, RT, OQ, S1, S2, S3, S4, S5}
+
+public static class PlayerTagExtensions
+{
+    public static bool is_offensive_lineman_not_center(this PlayerTag player_tag)
+    {
+        return player_tag is PlayerTag.LT or PlayerTag.LG or PlayerTag.RG or PlayerTag.RT;
+    }
+}
+
+public enum Direction {RIGHT, LEFT, FORWARD}
+public enum DirectionStringLength { SINGLE_LETTER, SHORT, FULL}
+public enum DirectionStringCapitalization { FIRST_LETTER, LOWER_CASE, UPPER_CASE}
+
+public static class DirectionExtensions
+{
+    public static float dir_factor(this Direction direction)
+    {
+        return direction == Direction.RIGHT ? 1 : -1;
+    }
+    
+    public static Direction opposite(this Direction direction)
+    {
+        return direction == Direction.RIGHT ? Direction.LEFT : Direction.RIGHT;
+    }
+
+    public static string get_direction_str(this Direction direction, DirectionStringLength direction_string_length, DirectionStringCapitalization direction_string_capitalization)
+    {
+        if (direction == Direction.RIGHT)
+        {
+            return direction_string_length switch
+            {
+                DirectionStringLength.SINGLE_LETTER => direction_string_capitalization switch
+                {
+
+                    DirectionStringCapitalization.FIRST_LETTER => "R",
+                    DirectionStringCapitalization.LOWER_CASE => "r",
+                    DirectionStringCapitalization.UPPER_CASE => "R",
+                    _ => throw new ArgumentOutOfRangeException(nameof(direction_string_capitalization), direction_string_capitalization, null)
+                },
+                DirectionStringLength.SHORT => direction_string_capitalization switch
+                {
+
+                    DirectionStringCapitalization.FIRST_LETTER => "Rt",
+                    DirectionStringCapitalization.LOWER_CASE => "rt",
+                    DirectionStringCapitalization.UPPER_CASE => "RT",
+                    _ => throw new ArgumentOutOfRangeException(nameof(direction_string_capitalization), direction_string_capitalization, null)
+                },
+                _ => direction_string_capitalization switch
+                {
+
+                    DirectionStringCapitalization.FIRST_LETTER => "Right",
+                    DirectionStringCapitalization.LOWER_CASE => "right",
+                    DirectionStringCapitalization.UPPER_CASE => "RIGHT",
+                    _ => throw new ArgumentOutOfRangeException(nameof(direction_string_capitalization), direction_string_capitalization, null)
+                }
+            };
+
+        }
+        
+        return direction_string_length switch
+        {
+            DirectionStringLength.SINGLE_LETTER => direction_string_capitalization switch
+            {
+
+                DirectionStringCapitalization.FIRST_LETTER => "L",
+                DirectionStringCapitalization.LOWER_CASE => "l",
+                DirectionStringCapitalization.UPPER_CASE => "L",
+                _ => throw new ArgumentOutOfRangeException(nameof(direction_string_capitalization), direction_string_capitalization, null)
+            },
+            DirectionStringLength.SHORT => direction_string_capitalization switch
+            {
+
+                DirectionStringCapitalization.FIRST_LETTER => "Lt",
+                DirectionStringCapitalization.LOWER_CASE => "lt",
+                DirectionStringCapitalization.UPPER_CASE => "LT",
+                _ => throw new ArgumentOutOfRangeException(nameof(direction_string_capitalization), direction_string_capitalization, null)
+            },
+            _ => direction_string_capitalization switch
+            {
+
+                DirectionStringCapitalization.FIRST_LETTER => "Left",
+                DirectionStringCapitalization.LOWER_CASE => "left",
+                DirectionStringCapitalization.UPPER_CASE => "LEFT",
+                _ => throw new ArgumentOutOfRangeException(nameof(direction_string_capitalization), direction_string_capitalization, null)
+            }
+        };
+    }
+}
+
+public class Player
+{
+    public PlayerTag player_tag;
+    public PlayerSide player_side;
+    public Vector2 pos;
+    
+    public Player(PlayerTag player_tag, PlayerSide player_side, float x = 0, float y = 0)
+    {
+        this.player_tag = player_tag;
+        this.player_side = player_side;
+        pos.X = x;
+        pos.Y = y;
+    }
+
+    public void set_position(float x, float y)
+    {
+        pos.X = x;
+        pos.Y = y;
+    }
+    
+    public void set_position(Vector2 position)
+    {
+        pos = position;
+    }
+}
+
+
+
+*/
