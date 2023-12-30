ef3dac679521664d39c6cdf7f4bad16700579276
MIKE-DESKTOP\Mike <aceplace1116@gmail.com>
2023-12-30 10:35:58 -0500

    Started to migrate field drawing and formation library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,328 @@
-#include "scm/common.cpp"
-#include "scm/formation.cpp"
-#include "scm/formation_utils.cpp"
-#include "raylib.h"
 #define RAYGUI_IMPLEMENTATION
+#include "raylib.h"
 #include "raygui.h"
 #include "rlgl.h"
 #include "raymath.h"
 
-enum class FieldType { HIGH_SCHOOL, COLLEGE, NFL };
+#include <vector>
+
+#include "scm/utils.cpp"
+#include "scm/common.cpp"
+#include "scm/formation.cpp"
+#include "scm/formation_utils.cpp"
+
+using namespace SCM24;
 
 Vector2 v2(float x, float y)
 {
     Vector2 result = { x, y };
     return result;
 }
+
+enum class PlayerFieldGuiMode 
+{
+    DEFAULT,
+    PANNING,
+    RECTANGLE_SELECT,
+    MOVING_PLAYERS
+};
+
+struct SelectedPlayerInfo
+{
+    Player *player;
+    Vector2 prev_pos;
+};
+
+struct PlayerFieldGuiState 
+{
+    float s_x1;
+    float s_y1;
+    float s_x2;
+    float s_y2;
+
+    std::vector<SelectedPlayerInfo> selected_players;    
+};
+
+bool contains_player(std::vector<SelectedPlayerInfo> selected_player_infos, Player* player)
+{
+    for (int i = 0; i < selected_player_infos.size(); i++)
+    {
+        if (selected_player_infos[i].player == player)
+            return true;
+    }
+    
+    return false;
+}
+
+SelectedPlayerInfo* get_selected_player_info_for_player(std::vector<SelectedPlayerInfo> selected_player_infos, Player *player)
+{
+    for (SelectedPlayerInfo& selected_player_info: selected_player_infos)
+    {
+        if (selected_player_info.player == player)
+            return &selected_player_info;
+    }
+    return nullptr;
+}
+
+void remove_player(std::vector<SelectedPlayerInfo> selected_player_infos, Player* player)
+{
+    for (int i = 0; i < selected_player_infos.size(); i++)
+    {
+        if (selected_player_infos[i].player == player)
+        {
+            selected_player_infos.erase(selected_player_infos.begin() + i);
+            break;
+        }
+    }
+}
+
+class PlayerFieldGui 
+{
+    const float PLAYER_WIDTH_OFFENSE = 2.5f;
+    const float PLAYER_HEIGHT_OFFENSE = 0.6f;
+    const float TAG_WIDTH_OFFENSE = 1.4f;
+    const float TAG_HEIGHT_OFFENSE = 0.6f;
+    const float TAG_WIDTH_DEFENSE = 1.6f;
+    const float TAG_HEIGHT_DEFENSE = 0.8f;
+    
+    const Color PLAYER_UNSELECTED_COLOR_OFFENSE = LIGHTGRAY;
+    const Color PLAYER_SELECTED_COLOR_OFFENSE = SKYBLUE;
+    const Color PLAYER_UNSELECTED_COLOR_DEFENSE = BLACK;
+    const Color PLAYER_SELECTED_COLOR_DEFENSE = RED;
+
+public:
+    PlayerFieldGuiState player_field_gui_state;
+    Vector2 _mouse_pos_field;
+    std::vector<Player*> gui_players;
+    bool was_in_default_state;
+
+    bool is_mouse_position_within_player(Player* found_player)
+    {
+        for (int i = 0; i < gui_players.size(); i++)
+        {
+            if (point_in_ellipse(_mouse_pos_field, v2(gui_players[i]->pos.x, gui_players[i]->pos.y), PLAYER_WIDTH_OFFENSE / 2.0, PLAYER_HEIGHT_OFFENSE / 2.0))
+            {
+                found_player = gui_players[i];
+                return true;
+            }
+        }
+
+        found_player = nullptr;
+        return false;
+    }
+
+    void draw_player(Player *player/*, Color4? dot_color_for_player*/)
+    {
+        Color player_color;
+        if (player->player_side == PlayerSide::OFFENSE)
+            player_color = contains_player(player_field_gui_state.selected_players, player) ? PLAYER_SELECTED_COLOR_OFFENSE : PLAYER_UNSELECTED_COLOR_OFFENSE;
+        else
+            player_color = contains_player(player_field_gui_state.selected_players, player) ? PLAYER_SELECTED_COLOR_DEFENSE : PLAYER_UNSELECTED_COLOR_DEFENSE;
+
+        if (player->player_side == PlayerSide::OFFENSE)
+        {
+            DrawEllipse(player->pos.x, player->pos.y, PLAYER_WIDTH_OFFENSE, PLAYER_HEIGHT_OFFENSE, player_color);
+            // MCYGraphics.draw_immediate_text(PlayerFieldGuiConstants.PLAYER_LABEL_MAP[player.player_tag], player.pos.X, player.pos.Y, PlayerFieldGuiConstants.TAG_WIDTH_OFFENSE, PlayerFieldGuiConstants.TAG_HEIGHT_OFFENSE, Color4.Black);
+            // if (dot_color_for_player != null)
+            //     MCYGraphics.draw_immediate_rectangle(player.pos.X, player.pos.Y, 0.3f, 0.3f, dot_color_for_player.Value);        
+        }
+        else
+        {
+            // MCYGraphics.draw_immediate_text(PlayerFieldGuiConstants.PLAYER_LABEL_MAP[player.player_tag], player.pos.X, player.pos.Y, PlayerFieldGuiConstants.TAG_WIDTH_DEFENSE, PlayerFieldGuiConstants.TAG_HEIGHT_DEFENSE, player_color);
+        }
+    }
+
+    void handle_input_for_field_and_render_select_box()
+    {
+
+    }
+
+    virtual void update_and_render()
+    {
+        handle_input_for_field_and_render_select_box();
+        
+        for (Player* player : gui_players)
+            draw_player(player);
+    }
+};
+
+class FormationEditGui : PlayerFieldGui
+{
+public:
+    // const float _FORMATION_DETAILS_HEIGHT = 115f;
+    // const float _AFFECTED_PLAYER_CHECKBOX_SPACING = 10;
+    // const float _LIST_BOX_WIDTH = 200.0f;    
+    std::vector<Player> _field_players;
+    std::vector<Player> _mof_players;
+    std::vector<Player> _boundary_players;
+    // SCMGui _scm_gui;
+    // float _menubar_height;
+
+    // int _formations_list_selection_index;
+    FormationLibrary* _formation_library;
+    std::string _formation_library_path;
+    std::string _formation_library_save_message;
+    // std::vector<std::string[]> _formation_listbox_items;   
+    std::string _formation_name = "";
+    std::string _formation_tags = "";
+    bool _lt_affected;
+    bool _lg_affected;
+    bool _oc_affected;
+    bool _rg_affected;
+    bool _rt_affected;
+    bool _oq_affected;
+    bool _s1_affected;
+    bool _s2_affected;
+    bool _s3_affected;
+    bool _s4_affected;
+    bool _s5_affected;
+
+    FormationEditGui(/*SCMGui scm_gui,*/ FormationLibrary* formation_library/*, std::string formation_library_path, std::string formation_library_save_message*/)
+    {
+        // _scm_gui = scm_gui;
+        _field_players = create_offense_and_or_defense_players(true, false);
+        _mof_players = create_offense_and_or_defense_players(true, false);
+        _boundary_players = create_offense_and_or_defense_players(true, false);
+        _formation_library = formation_library;
+        // _formation_library_path = formation_library_path;
+        // _formation_library_save_message = formation_library_save_message;
+
+        #pragma region Field Player Default Positions in Editor
+        for (Player& player: _field_players)
+        {
+            switch (player.player_tag)
+            {
+                case PlayerTag::LT:
+                    player.set_position(-26, -1);
+                    break;
+                case PlayerTag::LG:
+                    player.set_position(-22, -1);
+                    break;
+                case PlayerTag::OC:
+                    player.set_position(-18, -1);
+                    break;
+                case PlayerTag::RG:
+                    player.set_position(-14, -1);
+                    break;
+                case PlayerTag::RT:
+                    player.set_position(-10, -1);
+                    break;
+                case PlayerTag::OQ:
+                    player.set_position(-18, -2);
+                    break;
+                case PlayerTag::S1:
+                    player.set_position(-18, -7);
+                    break;
+                case PlayerTag::S2:
+                    player.set_position(-18, -5);
+                    break;
+                case PlayerTag::S3:
+                    player.set_position(-40, -1);
+                    break;
+                case PlayerTag::S4:
+                    player.set_position( -6, -1);
+                    break;
+                case PlayerTag::S5:
+                    player.set_position( 20, -2);
+                    break;
+            }
+            gui_players.push_back(&player);
+            player.pos = Vector2Add(player.pos, v2(0, 15));
+        }
+        #pragma endregion
+        
+        #pragma region MOF Player Default Positions in Editor
+        for (Player& player: _mof_players)
+        {
+            switch (player.player_tag)
+            {
+                case PlayerTag::LT:
+                    player.set_position( -8, -1);
+                    break;
+                case PlayerTag::LG:
+                    player.set_position( -4, -1);
+                    break;
+                case PlayerTag::OC:
+                    player.set_position(  0, -1);
+                    break;
+                case PlayerTag::RG:
+                    player.set_position(  4, -1);
+                    break;
+                case PlayerTag::RT:
+                    player.set_position(  8, -1);
+                    break;
+                case PlayerTag::OQ:
+                    player.set_position(  0, -2);
+                    break;
+                case PlayerTag::S1:
+                    player.set_position(  0, -7);
+                    break;
+                case PlayerTag::S2:
+                    player.set_position(  0, -5);
+                    break;
+                case PlayerTag::S3:
+                    player.set_position(-36, -1);
+                    break;
+                case PlayerTag::S4:
+                    player.set_position( 12, -1);
+                    break;
+                case PlayerTag::S5:
+                    player.set_position( 36, -2);
+                    break;
+            }
+            gui_players.push_back(&player);
+        }
+        #pragma endregion
+
+        #pragma region Boundary Player Default Positions in Editor
+        for (Player player: _boundary_players)
+        {
+            switch (player.player_tag)
+            {
+                case PlayerTag::LT:
+                    player.set_position( 10, -1);
+                    break;
+                case PlayerTag::LG:
+                    player.set_position( 14, -1);
+                    break;
+                case PlayerTag::OC:
+                    player.set_position( 18, -1);
+                    break;
+                case PlayerTag::RG:
+                    player.set_position( 22, -1);
+                    break;
+                case PlayerTag::RT:
+                    player.set_position(  26, -1);
+                    break;
+                case PlayerTag::OQ:
+                    player.set_position(  18, -2);
+                    break;
+                case PlayerTag::S1:
+                    player.set_position(  18, -7);
+                    break;
+                case PlayerTag::S2:
+                    player.set_position(  18, -5);
+                    break;
+                case PlayerTag::S3:
+                    player.set_position(-20, -1);
+                    break;
+                case PlayerTag::S4:
+                    player.set_position( 30, -1);
+                    break;
+                case PlayerTag::S5:
+                    player.set_position( 40, -2);
+                    break;
+            }
+            gui_players.push_back(&player);
+            player.pos = Vector2Add(player.pos, v2(0, -15));
+        }
+        #pragma endregion
+    }
+};
+
+enum class FieldType { HIGH_SCHOOL, COLLEGE, NFL };
+
+
 
 void draw_field(FieldType field_type) 
 {
@@ -116,6 +425,10 @@ int main() {
     SetTargetFPS(60);
 
     Rectangle window_area = {0, 0, 200, 30};
+
+    FormationLibrary formation_library;
+
+    FormationEditGui formation_edit_gui(&formation_library);
 
     while (!WindowShouldClose()) {
         BeginDrawing();
--- src/scm/utils.h
+++ src/scm/utils.h
@@ -2,32 +2,12 @@
 #include <raylib.h>
 #include "common.h"
 #include <vector>
-#include <stdexcept>
-#include <sstream>
-#include <cassert>
 
 namespace SCM24
 {
 
-Vector2 get_mirrored_x_vector_if(Vector2 value, bool mirror_x)
-{
-    value.x = mirror_x ? value.x * -1 : value.x;
-    return value;
-}
-
-Player& get_player_with_tag(std::vector<Player>& players, PlayerTag player_tag)
-{
-    for (Player& player: players)
-    {
-        if (player.player_tag == player_tag)
-        {
-            return player;
-        }
-    }
-    
-    std::ostringstream err_msg;
-    err_msg << "Player tag " << static_cast<int>(player_tag) << " doesn't appear in the list of players";
-    throw std::invalid_argument(err_msg.str());
-}
+Vector2 get_mirrored_x_vector_if(Vector2 value, bool mirror_x);
+Player& get_player_with_tag(std::vector<Player>& players, PlayerTag player_tag);
+bool point_in_ellipse(Vector2 pos, Vector2 pos_ellipse_center, float a, float b);
 
 }
